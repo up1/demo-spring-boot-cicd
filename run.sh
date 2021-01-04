28991910c0c86fe44606b66e33b5e849519d66a4
up1 <somkiat.p@gmail.com>
2021-01-04 23:27:47 +0700

    wait to start

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ APP_NAME=demo
 APP_JAR=$APP_NAME".jar"
 nohup java -jar $APP_JAR > $APP_NAME".log" 2>&1 &
 echo $! > $APP_NAME".tpid"
-while ! timeout 1 bash -c "echo > /dev/tcp/localhost/13000"; do sleep 3; done
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/8080"; do sleep 3; done
 echo $APP_NAME Start Success!
